fb51ccd247ee14e29cf01e5f0073d16a48d0a617
Will <willwaltrip@gmail.com>
2026-02-01 17:56:58 -0500

    finished contract with hardcoded values, and implemented tests.

--- week_6/contracts/src/Bilinear_Pairings.sol
+++ week_6/contracts/src/Bilinear_Pairings.sol
@@ -28,17 +28,7 @@ contract BilinearPairings {
     uint256 constant G2_x0 = 10857046999023057135944570762232829481370756359578518086990519993285655852781;                                                                                                                                                       
     uint256 constant G2_x1 = 11559732032986387107991004021392285783925812861821192530917403151452391805634;                                                                                                                                                         
     uint256 constant G2_y0 = 8495653923123431417604973247489272438418190587263600148770280649306958101930;                                                                                                                                                          
-    uint256 constant G2_y1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531; 
-
-    // neg(G1 * a) where a = 2                                                                                                                                                                                                                     
-    uint256 constant NEG_G1_A_X = 1368015179489954701390400359078579693043519447331113978918064868415326638035;                                                                                                                                    
-    uint256 constant NEG_G1_A_Y = 11970132820537103637166003141937572314130795164147247315533067598634108082819;                                                                                                                                   
-                                                                                                                                                                                                                                                    
-    // G2 * b where b = 13                                                                                                                                                                                                                         
-    uint256 constant G2_B_X0 = 16137324789686743234629608741537369181251990815455155257427276976918350071287;                                                                                                                                      
-    uint256 constant G2_B_X1 = 280672898440571232725436467950720547829638241593507531241322547969961007057;                                                                                                                                        
-    uint256 constant G2_B_Y0 = 12136420650226457477690750437223209427924916790606163705631661913973995426040;                                                                                                                                      
-    uint256 constant G2_B_Y1 = 17641806683785498955878869918183868440783188556637975525088932771694068429840;                                                                                                                                      
+    uint256 constant G2_y1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531;                                                                                                                                  
                                                                                                                                                                                                                                                     
     // G1 * alpha where alpha = 2                                                                                                                                                                                                                  
     uint256 constant G1_ALPHA_X = 1368015179489954701390400359078579693043519447331113978918064868415326638035;                                                                                                                                    
@@ -58,11 +48,7 @@ contract BilinearPairings {
     uint256 constant G2_GAMMA_X0 = 18029695676650738226693292988307914797657423701064905010927197838374790804409;                                                                                                                                  
     uint256 constant G2_GAMMA_X1 = 14583779054894525174450323658765874724019480979794335525732096752006891875705;                                                                                                                                  
     uint256 constant G2_GAMMA_Y0 = 2140229616977736810657479771656733941598412651537078903776637920509952744750;                                                                                                                                   
-    uint256 constant G2_GAMMA_Y1 = 11474861747383700316476719153975578001603231366361248090558603872215261634898;                                                                                                                                  
-                                                                                                                                                                                                                                                    
-    // G1 * c where c = 4                                                                                                                                                                                                                          
-    uint256 constant G1_C_X = 3010198690406615200373504922352659861758983907867017329644089018310584441462;                                                                                                                                        
-    uint256 constant G1_C_Y = 4027184618003122424972590350825261965929648733675738730716654005365300998076;                                                                                                                                        
+    uint256 constant G2_GAMMA_Y1 = 11474861747383700316476719153975578001603231366361248090558603872215261634898;                                                                                                                                                                                                                                                                     
                                                                                                                                                                                                                                                     
     // G2 * delta where delta = 2                                                                                                                                                                                                                  
     uint256 constant G2_DELTA_X0 = 18029695676650738226693292988307914797657423701064905010927197838374790804409;                                                                                                                                  
@@ -72,16 +58,53 @@ contract BilinearPairings {
 
 
     function runPairings(
-        G1_ECPoint memory A, 
-        G2_ECPoint memory B, 
-        G1_ECPoint memory C, 
-        uint256 x_1, 
-        uint256 x_2, 
-        uint256 x_3) public returns(bool, uint256, uint256) {
+        G1_ECPoint memory NEG_A,
+        G2_ECPoint memory B,
+        G1_ECPoint memory C,
+        uint256 x_1,
+        uint256 x_2,
+        uint256 x_3) public view returns(bool) {
 
             // need to generate X_1
-                // first generate points for X
+                // complete scalar multiplication for each x value
+                (uint256 x1G_x, uint256 x1G_y) = scalarMul(G1_x, G1_y, x_1);
+                (uint256 x2G_x, uint256 x2G_y) = scalarMul(G1_x, G1_y, x_2);
+                (uint256 x3G_x, uint256 x3G_y) = scalarMul(G1_x, G1_y, x_3);
 
+                // add all three points together to derive X_1; this will require adding first two then adding third to the result of the first operation
+                (uint256 sum_x1x2_x, uint256 sum_x1x2_y) = ecAddition(x1G_x, x1G_y, x2G_x, x2G_y);
+                (uint256 X1_x, uint256 X1_y) = ecAddition(sum_x1x2_x, sum_x1x2_y, x3G_x, x3G_y);
+            
+            // Then perform the bilinear pairing
+            // G2 points must be in order: (x1, x0, y1, y0)
+            uint256[24] memory input = [
+                NEG_A.x,
+                NEG_A.y,
+                B.x1,
+                B.x0,
+                B.y1,
+                B.y0,
+                G1_ALPHA_X,
+                G1_ALPHA_Y,
+                G2_BETA_X1,
+                G2_BETA_X0,
+                G2_BETA_Y1,
+                G2_BETA_Y0,
+                X1_x,
+                X1_y,
+                G2_GAMMA_X1,
+                G2_GAMMA_X0,
+                G2_GAMMA_Y1,
+                G2_GAMMA_Y0,
+                C.x,
+                C.y,
+                G2_DELTA_X1,
+                G2_DELTA_X0,
+                G2_DELTA_Y1,
+                G2_DELTA_Y0
+            ];
+
+            return pairings(input);
     }
 
     // used to perform the ecAddition
@@ -121,15 +144,11 @@ contract BilinearPairings {
     }
 
     // function checks is a set of pairings is valid
-    function pairings(uint256[12] memory input) internal view returns(bool) {
-        assembly{
-            let success := staticcall(gas(), 0x08, input, 0x0180, output, 0x020)
-            if success {
-                return(input, 0x20)
-            }
-        }
-
-        revert("Wrong pariing");
+    function pairings(uint256[24] memory input) internal view returns(bool) {
+        (bool success, bytes memory out) = address(0x08).staticcall(abi.encodePacked(input));
+        require(success && out.length == 32, "pairing failed");
+        return abi.decode(out, (uint256)) == 1;
+        
     }
 
     /**
--- week_6/contracts/test/Bilinear_Pairings.t.sol
+++ week_6/contracts/test/Bilinear_Pairings.t.sol
@@ -1,50 +1,106 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {BilinearPairings, Point} from "../src/Bilinear_Pairings.sol";
+import {BilinearPairings} from "../src/Bilinear_Pairings.sol";
 
 contract PairingsTest is Test {
     BilinearPairings public pairing;
 
-    function setup() public {
+    function setUp() public {
         pairing = new BilinearPairings();
     }
 
-    function test_CheckPairings() public view {
+    function test_ValidPairings() public {
+        // Test values from Python script output
+        // Based on: a=2, b=13, c=4, x_1=1, x_2=2, x_3=3
+        // alpha=2, beta=3, gamma=2, delta=2
 
-        // A value 
-        uint256 aG1_x = 0;
-        uint256 aG1_y = 0;
+        // neg(G1 * a) where a = 2
+        BilinearPairings.G1_ECPoint memory NEG_A = BilinearPairings.G1_ECPoint({
+            x: 1368015179489954701390400359078579693043519447331113978918064868415326638035,
+            y: 11970132820537103637166003141937572314130795164147247315533067598634108082819
+        });
 
-        // B Value
-        uint256 bG2_x1 = 0;
-        uint256 bG2_x2 = 0;
-        uint256 bG2_y1 = 0;
-        uint256 bG2_y2 = 0;
+        // G2 * b where b = 13
+        BilinearPairings.G2_ECPoint memory B = BilinearPairings.G2_ECPoint({
+            x0: 16137324789686743234629608741537369181251990815455155257427276976918350071287,
+            x1: 280672898440571232725436467950720547829638241593507531241322547969961007057,
+            y0: 12136420650226457477690750437223209427924916790606163705631661913973995426040,
+            y1: 17641806683785498955878869918183868440783188556637975525088932771694068429840
+        });
 
-        // C value
-        uint256 cG1_x = 0;
-        uint256 cG1_y = 0;
+        // G1 * c where c = 4
+        BilinearPairings.G1_ECPoint memory C = BilinearPairings.G1_ECPoint({
+            x: 3010198690406615200373504922352659861758983907867017329644089018310584441462,
+            y: 4027184618003122424972590350825261965929648733675738730716654005365300998076
+        });
 
-        // X_1 value
-        uint256 xG1_x = 0;
-        uint256 xG1_y = 0;
-
-        // x values 
+        // x values (will be used to compute X_1 = x_1*G1 + x_2*G1 + x_3*G1)
         uint256 x_1 = 1;
         uint256 x_2 = 2;
         uint256 x_3 = 3;
 
-        (bool result, uint256 X1_x, uint256 X2_y) = pairing.runPairings(
-            G1_ECPoint(aG1_x, aG1_y),
-            G2_ECPoint(bG2_x1, bG2_x2, bG2_y1, bG2_y2),
-            G1_ECPoint(cG1_x, cG1_y),
-            x_1, 
-            x_2, 
-            x_3
-        );
+        bool result = pairing.runPairings(NEG_A, B, C, x_1, x_2, x_3);
 
-        assertEq(result, true);
+        assertEq(result, true, "Valid pairing should return true");
+    }
 
+    function test_InvalidPairings_WrongA() public {
+        // Use wrong A value - should fail
+        BilinearPairings.G1_ECPoint memory WRONG_NEG_A = BilinearPairings.G1_ECPoint({
+            x: 1,
+            y: 2
+        });
+
+        // Correct B value
+        BilinearPairings.G2_ECPoint memory B = BilinearPairings.G2_ECPoint({
+            x0: 16137324789686743234629608741537369181251990815455155257427276976918350071287,
+            x1: 280672898440571232725436467950720547829638241593507531241322547969961007057,
+            y0: 12136420650226457477690750437223209427924916790606163705631661913973995426040,
+            y1: 17641806683785498955878869918183868440783188556637975525088932771694068429840
+        });
+
+        // Correct C value
+        BilinearPairings.G1_ECPoint memory C = BilinearPairings.G1_ECPoint({
+            x: 3010198690406615200373504922352659861758983907867017329644089018310584441462,
+            y: 4027184618003122424972590350825261965929648733675738730716654005365300998076
+        });
+
+        uint256 x_1 = 1;
+        uint256 x_2 = 2;
+        uint256 x_3 = 3;
+
+        bool result = pairing.runPairings(WRONG_NEG_A, B, C, x_1, x_2, x_3);
+
+        assertEq(result, false, "Invalid pairing should return false");
+    }
+
+    function test_InvalidPairings_WrongXValues() public {
+        // Use correct A, B, C but wrong x values
+        BilinearPairings.G1_ECPoint memory NEG_A = BilinearPairings.G1_ECPoint({
+            x: 1368015179489954701390400359078579693043519447331113978918064868415326638035,
+            y: 11970132820537103637166003141937572314130795164147247315533067598634108082819
+        });
+
+        BilinearPairings.G2_ECPoint memory B = BilinearPairings.G2_ECPoint({
+            x0: 16137324789686743234629608741537369181251990815455155257427276976918350071287,
+            x1: 280672898440571232725436467950720547829638241593507531241322547969961007057,
+            y0: 12136420650226457477690750437223209427924916790606163705631661913973995426040,
+            y1: 17641806683785498955878869918183868440783188556637975525088932771694068429840
+        });
+
+        BilinearPairings.G1_ECPoint memory C = BilinearPairings.G1_ECPoint({
+            x: 3010198690406615200373504922352659861758983907867017329644089018310584441462,
+            y: 4027184618003122424972590350825261965929648733675738730716654005365300998076
+        });
+
+        // Wrong x values
+        uint256 x_1 = 5;
+        uint256 x_2 = 6;
+        uint256 x_3 = 7;
+
+        bool result = pairing.runPairings(NEG_A, B, C, x_1, x_2, x_3);
+
+        assertEq(result, false, "Invalid x values should return false");
     }
 }
